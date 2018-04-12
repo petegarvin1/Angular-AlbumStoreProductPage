58160141fff8bd749e2fa03780f0a7989b3dda0e
Pete <petergarvin1@gmail.com>
2018-04-12 09:02:32 +0100

    fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,7 @@ export class ProductService {
 
 
 
-private _albumUrl = ' ../assets/album.json';
+private _albumUrl = "../assets/album.json";
 
 getAlbum(id: number) {
   return this._http.get(this._albumUrl).map((response) => response.json());
